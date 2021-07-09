bf21b348fc4c8c98854fe5c676ee0abd4863f811
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-09 10:28:07 -0500

    created function to add a new role

--- server.js
+++ server.js
@@ -330,3 +330,62 @@ function promptEmployeeRole(employeeChoices, roleChoices) {
                 });
         });
 }
+
+//add role
+//asks what role is being added
+function addRole() {
+    var query =
+        `SELECT d.id, d.name, r.salary AS budget
+            FROM employee e
+                JOIN role r
+                    ON e.role_id = r.id
+                JOIN department d
+                    ON d.id = r.department_id
+                GROUP BY d.id, d.name`
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        const departmentChoices = res.map(({ id, name }) => ({
+            value: id,
+            name: `${id} ${name}`
+        }));
+        console.table(res);
+        console.log("Adding New Role!");
+        promptAddRole(departmentChoices);
+    });
+}
+
+//list of questions
+function promptAddRole(departmentChoices) {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "roleTitle",
+                message: "Title Of New Role:"
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "Salary:"
+            },
+            {
+                type: "list",
+                name: "departmentId",
+                message: "Department:",
+                choices: departmentChoices
+            },
+        ])
+        .then(function(answer) {
+            var query = `INSERT INTO role SET ?`
+            connection.query(query, {
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: answer.departmentId
+                },
+                function(err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    console.log("Role Added!");
+                    firstPrompt();
+                });
+        });
+}
