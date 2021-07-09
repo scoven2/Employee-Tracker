c31a7f2a74d0215cf72e94a44052e05b6b7f799e
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-09 00:35:49 -0500

    created function to view all employees

--- server.js
+++ server.js
@@ -90,3 +90,23 @@ function firstPrompt() {
             }
         });
 }
+
+//view employees
+function viewEmployee() {
+    console.log("Viewing Employees\n");
+    var query =
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee e
+            LEFT JOIN role r
+                ON e.role_id = r.id
+            LEFT JOIN department d
+                ON d.id = r.department_id
+            LEFT JOIN employee m
+                ON m.id = e.manager_id`
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        console.log("Employees Listed Above.\n");
+        firstPrompt();
+    });
+}
